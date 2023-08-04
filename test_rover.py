eceaf9bbcecbf8ca6c9d367d3e8f459035f6dbed
Ben Nagy <nagy.benb@gmail.com>
2023-08-04 11:38:25 +0100

    wip

--- test_rover.py
+++ test_rover.py
@@ -13,47 +13,26 @@ from rover import *
 #   W   E
 #     S
 #
-def test_returns_initial_position_when_no_commands_given():
+def test_no_movement():
     assert rove((1, 1), "N", "") == (1, 1)
     assert rove((2, 2), "N", "") == (2, 2)
 
 
-def test_returns_y_incremented_by_one_given_this_coordinate():
+def test_forwards():
     assert rove((2, 2), "N", "f") == (2, 1)
-
-
-def test_returns_y_decremented_by_one_given_this_coordinate():
-    assert rove((2, 2), "N", "b") == (2, 3)
-
-
-def test_returns_y_incremented_by_one_given_this_coordinate_xxxxxxxx():
     assert rove((2, 2), "W", "f") == (1, 2)
-
-
-def test_returns_x_incremented_by_one_given_facing_east_with_this_coordinate_xxxxxxxx():
     assert rove((2, 2), "E", "f") == (3, 2)
-
-
-def test_returns_y_incremented_by_one_given_facing_south_with_this_coordinate_xxxxxxxx():
     assert rove((2, 2), "S", "f") == (2, 3)
-
-
-def test_returns_y_incremented_by_two_given_facing_south_and_the_command_ff():
     assert rove((2, 2), "S", "ff") == (2, 4)
+
+
+def test_backwards():
+    assert rove((2, 2), "N", "b") == (2, 3)
     assert rove((2, 2), "S", "bb") == (2, 0)
 
 
-def test_turning_with_no_movement_returns_initial_coordinates():
+def test_turning_left():
     assert rove((1, 1), "N", "llrr") == (1, 1)
-
-
-def test_returns_x_decremented_by_one_when_turning_left_and_going_forwards():
     assert rove((2, 2), "N", "lf") == (1, 2)
-
-
-def test_returns_x_decremented_by_one_when_turning_left_and_going_forwards_():
     assert rove((2, 2), "E", "lf") == (2, 1)
-
-
-def test_turning_left_when_asdfasdf():
     assert rove((2, 2), "S", "lf") == (3, 2)
