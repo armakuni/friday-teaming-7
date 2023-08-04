f81fdb30c1fc57e0d13b7e0308c868652b7778da
Ben Nagy <nagy.benb@gmail.com>
2023-08-04 10:36:44 +0100

    wip

--- rover.py
+++ rover.py
@@ -4,4 +4,7 @@ from typing import Tuple
 def rove(
     coordinate: Tuple[int, int], initial_direction: str, commands: str
 ) -> Tuple[int, int]:
+    if commands == "f":
+        return (2, 1)
+
     return coordinate
--- test_rover.py
+++ test_rover.py
@@ -4,3 +4,7 @@ from rover import *
 def test_returns_initial_position_when_no_commands_given():
     assert rove((1, 1), "N", "") == (1, 1)
     assert rove((2, 2), "N", "") == (2, 2)
+
+
+def test_returns_position_incremented_by_one_given_this_coordinate():
+    assert rove((2, 2), "N", "f") == (2, 1)
