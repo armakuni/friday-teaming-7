9579fb71c7c6e71528537c8fb3d2bd26e883f86e
Ben Nagy <nagy.benb@gmail.com>
2023-08-04 10:33:11 +0100

    wip

--- test_rover.py
+++ test_rover.py
@@ -1,9 +1,6 @@
 from rover import *
 
 
-def test_can_call_empty_function():
+def test_returns_initial_position_when_no_commands_given():
     assert rove((1, 1), "N", "") == (1, 1)
-
-
-def test_can_call_empty_function_1():
     assert rove((2, 2), "N", "") == (2, 2)
